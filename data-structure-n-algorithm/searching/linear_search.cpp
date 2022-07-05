81e08def06ff879db6b170cbd75638a3b583e315
Chuong Nguyen <54233434+ChuongNg1996@users.noreply.github.com>
2022-07-05 09:32:04 +0700

    Add files via upload

---
+++ data-structure-n-algorithm/searching/linear_search.cpp
@@ -0,0 +1,66 @@
+/*
+Linear search: Every item is checked and if a match is found then that item is returned, 
+otherwise the search continues till the end.
+*/
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main () {
+    // ---------------------------------------------------------- //
+    // ---------        File Variables                  --------- //
+    // ---------------------------------------------------------- //
+    ifstream indata; // indata is like cin
+    int num; // variable for input value
+    
+    // ---------------------------------------------------------- //
+    // ---------        Algorithm Variables             --------- //
+    // ---------------------------------------------------------- //
+    vector<int> storage;    // only C++
+    int max = 0;
+    int key;
+    int index;
+    bool found = 0;
+    cout << "Number to search for: ";
+    cin >> key;
+    // ---------------------------------------------------------- //
+    // ---------         Open & Store Unsorted Data     --------- //
+    // ---------------------------------------------------------- //
+    indata.open("dataset.txt"); // opens the file
+    if(!indata) { // file couldn't be opened
+        cerr << "Error: file could not be opened" << endl;
+        exit(1);
+    }
+    indata >> num;
+    while ( !indata.eof() ) { // keep reading until end-of-file
+        storage.push_back(num);
+        max++;
+        indata >> num; // sets EOF flag if no value found
+    }
+    indata.close();
+
+    // ---------------------------------------------------------- //
+    // ---------         Searching Algorrithm           --------- //
+    // --------------------------------------------------------- //
+
+    for (int i = 0; i < max; i++)   
+    {  
+        if (key == storage[i])
+        {
+            index = i;
+            found = 1;
+            break;
+        }
+    }
+
+    if (found) cout << "Found " << key << " at location " << index << endl;
+    else cout << key << " is not found" <<endl;
+
+    system("pause");
+    return 0;
+}
+/*
+Read File:          https://www.bgsu.edu/arts-and-sciences/computer-science/cs-documentation/reading-data-from-files-using-c-plus-plus.html 
+vector:             https://cplusplus.com/reference/vector/vector/ 
+*/

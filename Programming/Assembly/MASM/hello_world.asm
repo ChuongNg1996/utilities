fa143879dda1e89edfa7d6b4815dcb34a6f2fe31
Chuong Nguyen <54233434+ChuongNg1996@users.noreply.github.com>
2022-07-29 11:03:12 +0700

    Add files via upload

---
+++ Programming/Assembly/MASM/hello_world.asm
@@ -0,0 +1,5 @@
+    .equ    LAST_RAM_WORD,      0x007FFFFC
+    .equ    JATG_UART_BASE,     0x10001000
+    .equ    DATA_OFFSET,        0
+    .equ    STATUS_OFFSET,      4
+    .equ    WSPACE_MASK,        0xFFFF
